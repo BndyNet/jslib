a2527a4560afc8b7626cdb59bb257eca3dbe87de
Bndy <zb@bndy.net>
2016-11-25 22:02:57 +0800

    readonly for Form

--- src/js/init.coffee
+++ src/js/init.coffee
@@ -29,4 +29,35 @@
             loadingShade: [0.6, "#fff"]
             useAlertify: true
         
+    # read-only form
+    $("form.readonly").find("input,select,button").each ->
+        tag = $(this)[0].tagName
+        tagType = $(this).attr "type"
+        val = ""
+        switch tag
+            when "INPUT"
+                $(this).attr "readonly", true
+                $(this).attr "disabled", true
+                switch tagType
+                    when "radio", "checkbox"
+                        wrapper = $(this).parent()
+                        wrapper = wrapper.parent() if wrapper.parent().hasClass "checkbox" or wrapper.parent().hasClass "radio"
+                        if $(this).is ":checked"
+                            val = wrapper.text()
+                            wrapper.after "<div class='form-control-static'>#{val}</div>"
+                        wrapper.remove()
+                    when "file"
+                        wrapper = $(this).parents('.form-group')
+                        wrapper.remove()
+                    else
+                        val = $(this).val()
+                        $(this).after "<div class='form-control-static'>#{val}</div>"
+                        $(this).remove()
+            when "SELECT"
+                val = $(this).val()
+                $(this).after "<div class='form-control-static'>#{val}</div>"
+                $(this).remove()
+    $("form.readonly:not(:visible)").show()
+    
+    
     return
--- src/js/init.js
+++ src/js/init.js
@@ -34,6 +34,43 @@
         useAlertify: true
       });
     }
+    $("form.readonly").find("input,select,button").each(function() {
+      var tag, tagType, val, wrapper;
+      tag = $(this)[0].tagName;
+      tagType = $(this).attr("type");
+      val = "";
+      switch (tag) {
+        case "INPUT":
+          $(this).attr("readonly", true);
+          $(this).attr("disabled", true);
+          switch (tagType) {
+            case "radio":
+            case "checkbox":
+              wrapper = $(this).parent();
+              if (wrapper.parent().hasClass("checkbox" || wrapper.parent().hasClass("radio"))) {
+                wrapper = wrapper.parent();
+              }
+              if ($(this).is(":checked")) {
+                val = wrapper.text();
+                wrapper.after("<div class='form-control-static'>" + val + "</div>");
+              }
+              return wrapper.remove();
+            case "file":
+              wrapper = $(this).parents('.form-group');
+              return wrapper.remove();
+            default:
+              val = $(this).val();
+              $(this).after("<div class='form-control-static'>" + val + "</div>");
+              return $(this).remove();
+          }
+          break;
+        case "SELECT":
+          val = $(this).val();
+          $(this).after("<div class='form-control-static'>" + val + "</div>");
+          return $(this).remove();
+      }
+    });
+    $("form.readonly:not(:visible)").show();
   });
 
 }).call(this);
