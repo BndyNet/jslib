ca91487f306dbb26bccb9a13e955bedb9ed2fe7a
Bendy Zhang <zb@bndy.net>
2018-01-22 10:16:40 +0800

    $.fn.cover

--- jquery.ext.js
+++ jquery.ext.js
@@ -61,4 +61,40 @@ $.extend({
             $(this).css('overflow', 'hidden');
         }
     }
+
+    /**
+     * Sets a cover for current element.
+     * @function external:"jQuery.fn"#cover
+     * @param {string} msg - The cover html content.
+     * @returns {jQuery} The cover element if msg is not false, otherwise nothing.
+     * @example
+     * var loading = $('.box').cover('Loading...');
+     * loading.cover(false);    // remove cover
+     */
+    $.fn.cover = function(msg) {
+        var parent = $(this);
+        if (parent.hasClass('loading-wrapper')) {
+            parent = parent.parent();
+        }
+
+        if (msg || typeof msg === 'undefined') {
+            parent.css({position: 'relative'});
+            var elem = $('<div class="loading-wrapper"><span style="display: table-cell;vertical-align: middle;text-align: center;"></span></div>');
+            elem.find('span').html(msg);
+            elem.css({
+                position: 'absolute', 
+                top: 0, 
+                left: 0, 
+                display: 'table',
+                width: parent.outerWidth(), 
+                height: parent.outerHeight(),
+                backgroundColor: 'rgba(255,255,255, .8)',
+                textAlgin: 'center',
+            });
+            parent.append(elem);
+            return elem;
+        } else {
+            parent.find('.loading-wrapper').remove();
+        }
+    }
 })(jQuery);
